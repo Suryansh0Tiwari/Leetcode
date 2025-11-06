54c38c05926b1b2dca079734f3f304a20bd2d01f
Suryansh0Tiwari <91941517+Suryansh0Tiwari@users.noreply.github.com>
2025-11-06 23:15:14 +0530

    Time: 3 ms (76.92%), Space: 43.2 MB (100%) - LeetHub

---
+++ 0740-delete-and-earn/0740-delete-and-earn.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int deleteAndEarn(int[] nums) {
+        int [] bucket =new int[10001];
+
+        for(final int num : nums)
+            bucket[num]+=num;
+
+        int prev1=0,prev2=0;
+
+        for(final int num :bucket)
+        {
+            final int dp=Math.max(prev1,prev2+num);
+            prev2=prev1;
+            prev1=dp;
+        }
+
+        return prev1;
+    }
+}
