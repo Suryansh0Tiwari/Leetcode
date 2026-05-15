e029bb78f13a6eadf04948331a453447356c2e49
Suryansh0Tiwari <91941517+Suryansh0Tiwari@users.noreply.github.com>
2026-05-15 18:21:56 +0530

    Time: 3 ms (14.04%), Space: 43.2 MB (17.72%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.java
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.java
@@ -1,31 +1,32 @@
 class Solution {
-  public int splitArray(int[] nums, int m) {
-    int l = Arrays.stream(nums).max().getAsInt();
-    int r = Arrays.stream(nums).sum() + 1;
+    public int splitArray(int[] nums, int k) {
+        int l = Arrays.stream(nums).max().getAsInt();
+        int r = Arrays.stream(nums).sum() + 1;
 
-    while (l < r) {
-      final int mid = (l + r) / 2;
-      if (numGroups(nums, mid) > m)
-        l = mid + 1;
-      else
-        r = mid;
+        while (l < r) {
+            final int m = (l + r) / 2;
+            if (numGroups(nums, m) > k)
+                l = m + 1;
+            else
+                r = m;
+        }
+
+        return r;
     }
 
-    return l;
-  }
+    private int numGroups(int[] nums, int maxSumInGroup) {
+        int groupCount = 1;
+        int sumInGroup = 0;
 
-  private int numGroups(int[] nums, int maxSumInGroup) {
-    int groupCount = 1;
-    int sumInGroup = 0;
+        for (final int num : nums)
+            if (sumInGroup + num <= maxSumInGroup) {
+                sumInGroup += num;
+            } else {
+                ++groupCount;
+                sumInGroup = num;
+            }
 
-    for (final int num : nums)
-      if (sumInGroup + num <= maxSumInGroup) {
-        sumInGroup += num;
-      } else {
-        ++groupCount;
-        sumInGroup = num;
-      }
+        return groupCount;
+    }
 
-    return groupCount;
-  }
 }
